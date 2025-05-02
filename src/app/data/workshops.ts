50f4126a02ca73ca24d0ae045faaa0ae307fa7fb
Sebastian Ruiz <sebasrhrh@gmail.com>
2025-05-02 16:53:54 -0500

    feat(workshop): add workshops section and location

---
+++ src/app/data/workshops.ts
@@ -0,0 +1,17 @@
+export const workshops = [
+  {
+    title: "Taller 1 - Animación de mini series",
+    time: "8:10 - 9:25",
+    img: "/workshops/ani.webp"
+  },
+  {
+    title: "Taller 2 - Segmentación de Clientes a través del Algoritmo no Supervisado K-Means",
+    time: "9:25 - 10:40",
+    img: "/workshops/seg.webp"
+  },
+  {
+    title: "Taller 3 - ¡Del Terminal al Hype! Instalando Arch Linux y Creando tu Mundo con Hyprland",
+    time: "10:40 - 11:55",
+    img: "/workshops/hypr.webp"
+  },
+];
